da0d75dd31198fc612e846a4ab01fc2a24406d8a
NamJungGu <nowage@gmail.com>
2019-03-08 05:52:05 +0900

    pip install bug patch

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,8 +5,8 @@ if [ -e /etc/redhat-release ] ; then
   REDHAT_BASED=true
 fi
 
-TERRAFORM_VERSION="0.11.7"
-PACKER_VERSION="1.2.4"
+TERRAFORM_VERSION="0.11.11"
+PACKER_VERSION="1.3.5"
 # create new ssh key
 [[ ! -f /home/ubuntu/.ssh/mykey ]] \
 && mkdir -p /home/ubuntu/.ssh \
@@ -24,15 +24,18 @@ fi
 # add docker privileges
 usermod -G docker ubuntu
 # install pip
-pip install -U pip && pip3 install -U pip
+
+pip2 install -U pip && pip3 install -U pip
 if [[ $? == 127 ]]; then
-    wget -q https://bootstrap.pypa.io/get-pip.py
-    python get-pip.py
-    python3 get-pip.py
+    apt-get -y install python-pip
+    apt-get -y install python3-pip
+    # wget -q https://bootstrap.pypa.io/get-pip.py
+    # python get-pip.py
+    # python3 get-pip.py
 fi
 # install awscli and ebcli
-pip install -U awscli
-pip install -U awsebcli
+pip2 install -U awscli
+pip2 install -U awsebcli
 
 #terraform
 T_VERSION=$(/usr/local/bin/terraform -v | head -1 | cut -d ' ' -f 2 | tail -c +2)
